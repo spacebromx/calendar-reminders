2d9627adcc10f1249200fabe1afbef512cae747b
Alan Christopher Alamillo Medina <realconect@gmail.com>
2020-10-06 20:57:20 -0500

    Created the utility to generate an array with days of the month

---
+++ src/utils.js
@@ -0,0 +1,40 @@
+import getDaysInMonth from "date-fns/getDaysInMonth";
+import getDay from "date-fns/getDay";
+
+export const generateCalendar = ({month, year}) => {
+  // how many days are there in a month
+  const numberOfDaysInMonth = getDaysInMonth(new Date(year, month))
+
+  // how many days are there in the previous month
+  const numberOfDaysInPrevMonth = getDaysInMonth(new Date(year, month - 1 ))
+
+  // what day of the week is the first day of the month (0 - 6)
+  const startingDay = getDay(new Date(year, month , 1))
+  let days = Array(35).fill(0)
+
+  let j = 1
+  let k = 1
+  let l = 0
+  for(let i = 0; i < days.length; i++) {
+    if(i >= startingDay) {
+      if (j <= numberOfDaysInMonth ) {
+        days[i] = j
+        j++
+      } else {
+        days[i] = k
+        k++
+      }
+    } else {
+      l++
+    }
+  }
+
+  // Generate an array with the days of the month prior
+  const complement = [...Array(numberOfDaysInPrevMonth + 1).keys()].splice(-l)
+
+  for(let i = 0; i < l; i++) {
+    days[i] = complement[i]
+  }
+
+  return days
+}
