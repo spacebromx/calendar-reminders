cf11a3390a099265ce8e5e5cdcd26a7bb8efc812
Alan Christopher Alamillo Medina <realconect@gmail.com>
2020-10-07 00:17:48 -0500

    Modified the UI to match requirements

--- src/components/Day.js
+++ src/components/Day.js
@@ -6,9 +6,9 @@ const Day = ({number, highlight, current, today}) => {
   return (
     <div style={{width: "14.28%", height: "120px"}}
          className={cx('day px-4 pt-2 border-r border-b relative overflow-y-scroll', {'bg-gray-200': highlight})}>
-      <div
+      <button
         className={cx('inline-flex w-6 h-6 items-center justify-center cursor-pointer text-center leading-none rounded-full transition ease-in-out duration-100 text-gray-400 hover:bg-blue-200 font-bold', {'text-gray-700': current}, {'text-blue-600': current && highlight}, {'text-red-600 bg-red-200': today} )}>{number}
-      </div>
+      </button>
       <div style={{height: "80px"}} className="overflow-y-auto mt-1">
         {/*<div className="px-2 py-1 rounded-lg mt-1 overflow-hidden border" />*/}
       </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,8 +5,8 @@ const Header = () => {
   return (
     <div className="flex flex-wrap">
       {WEEKDAYS.map(day => (
-        <div className="px-2 py-2 flex-1" key={day}>
-          <div className="text-gray-600 text-sm uppercase tracking-wide font-bold text-center">{day}</div>
+        <div className="px-2 py-2 flex-1 bg-blue-400 text-white" key={day}>
+          <div className="text-gray-600 text-sm uppercase tracking-wide font-bold text-center text-blue-100">{day}</div>
         </div>
       ))}
     </div>
