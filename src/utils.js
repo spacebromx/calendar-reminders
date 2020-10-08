8cc1997e0d1a3a7330d99dd3ec13e4d15007753e
Alan Christopher Alamillo Medina <realconect@gmail.com>
2020-10-07 23:08:28 -0500

    Fixed ESLint issues in utils file

--- src/utils.js
+++ src/utils.js
@@ -1,85 +1,85 @@
 import getDaysInMonth from "date-fns/getDaysInMonth";
 import getDay from "date-fns/getDay";
-import {DATE_FORMAT_OPTIONS, TIME_FRAMES} from "./constants";
+import { DATE_FORMAT_OPTIONS, TIME_FRAMES } from "./constants";
 
-export const generateCalendar = ({month, year}) => {
+export const generateCalendar = ({ month, year }) => {
   // how many days are there in a month
-  const numberOfDaysInMonth = getDaysInMonth(new Date(year, month))
+  const numberOfDaysInMonth = getDaysInMonth(new Date(year, month));
 
   // how many days are there in the previous month
-  const numberOfDaysInPrevMonth = getDaysInMonth(new Date(year, month - 1))
+  const numberOfDaysInPrevMonth = getDaysInMonth(new Date(year, month - 1));
 
   // what day of the week is the first day of the month (0 - 6)
-  const startingDay = getDay(new Date(year, month, 1))
-  let days = Array(35).fill({})
+  const startingDay = getDay(new Date(year, month, 1));
+  let days = Array(35).fill({});
 
-  let j = 1
-  let k = 1
-  let l = 0
+  let j = 1;
+  let k = 1;
+  let l = 0;
   for (let i = 0; i < days.length; i++) {
     if (i >= startingDay) {
       if (j <= numberOfDaysInMonth) {
         days[i] = {
           date: formatDate(new Date(year, month, j)),
-          period: TIME_FRAMES.CURRENT
-        }
-        j++
+          period: TIME_FRAMES.CURRENT,
+        };
+        j++;
       } else {
         days[i] = {
           date: formatDate(new Date(year, month + 1, k)),
-          period: TIME_FRAMES.LATER
-        }
-        k++
+          period: TIME_FRAMES.LATER,
+        };
+        k++;
       }
     } else {
-      l++
+      l++;
     }
   }
 
   // Generate an array with the days of the month prior
-  const complement = [...Array(numberOfDaysInPrevMonth + 1).keys()].splice(-l)
+  const complement = [...Array(numberOfDaysInPrevMonth + 1).keys()].splice(-l);
 
   for (let i = 0; i < l; i++) {
     days[i] = {
       date: formatDate(new Date(year, month - 1, complement[i])),
-      period: TIME_FRAMES.PRIOR
-    }
+      period: TIME_FRAMES.PRIOR,
+    };
   }
 
-  return days
-}
+  return days;
+};
 
-export const getMonthName = ({month, year}) => {
+export const getMonthName = ({ month, year }) => {
   try {
-    const formatter = new Intl.DateTimeFormat('en', {month: 'long'});
-    return formatter.format(new Date(year, month))
+    const formatter = new Intl.DateTimeFormat("en", { month: "long" });
+    return formatter.format(new Date(year, month));
   } catch (e) {
-    console.error(e)
+    console.error(e);
   }
-}
+};
 
-export const getStringDate = ({year, month, day}) => {
-  const now = new Date(year, month, day)
+export const getStringDate = ({ year, month, day }) => {
+  const now = new Date(year, month, day);
 
-  return now.toLocaleString('en', DATE_FORMAT_OPTIONS)
-}
+  return now.toLocaleString("en", DATE_FORMAT_OPTIONS);
+};
 
-export const formatDate = date => date.toLocaleString('en')
+export const formatDate = (date) => date.toLocaleString("en");
 
 export const parseAndFormatDate = (date, options = DATE_FORMAT_OPTIONS) => {
   try {
-    const dateParsed = Date.parse(date)
-    return new Date(dateParsed).toLocaleString('en', options)
+    const dateParsed = Date.parse(date);
+    return new Date(dateParsed).toLocaleString("en", options);
   } catch (e) {
-    throw new Error('invalid date format')
+    throw new Error("invalid date format");
   }
-}
+};
 
 export const getRemindersByDate = (reminders, date) => {
-  let newDate = parseAndFormatDate(date)
-  return reminders.filter(reminder => reminder.date === newDate)
-}
+  let newDate = parseAndFormatDate(date);
+  return reminders.filter((reminder) => reminder.date === newDate);
+};
 
 export const getReminderById = (reminders, id) => {
-  return reminders.filter(reminder => reminder.id === id)
-}
+  return reminders.filter((reminder) => reminder.id === id);
+};
